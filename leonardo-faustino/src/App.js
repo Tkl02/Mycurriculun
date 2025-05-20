8ccba92c7b605179809dbdc1ebc3eefa857a2e22
Leonardo Faustino <84601712+Tkl02@users.noreply.github.com>
2025-05-20 14:06:04 -0300

    FEAT: Criação inicial da home page

--- leonardo-faustino/src/App.js
+++ leonardo-faustino/src/App.js
@@ -1,11 +1,15 @@
 import './App.css'
 import Footer from './Components/footer/Footer';
 import Taskbar from './Components/taskbar/Taskbar';
+import HomePage from './pages/homePage/HomePage'
+
+// pallete references: https://coolors.co/palette/131515-2b2c28-339989-7de2d1-fffafb
 
 function App() {
   return (
     <>
     <Taskbar/>
+    <HomePage/>
     <Footer/>
     </>
   )
